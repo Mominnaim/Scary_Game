61bc2adc0e1f77df6b6ba1f97c6fdb0d494f0620
Naim Momin <mominnaim11@gmail.com>
2023-07-30 03:21:16 -0400

    added a help option, added story in the beginning, made some minor tweaks

--- skeleton_scary_game.py
+++ skeleton_scary_game.py
@@ -1,5 +1,14 @@
 import random
 
+print("Narrator: The father and his daughter are on vacation in a log cabin in the forrest. ")
+print("Daddy: Would you like to go on a late night walk and clear our mind?")
+print("Sheena: yes daddy, the weather is really nice too!")
+print("Daddy: Make sure you don't leave my side ok, and do not let go of my hand.")
+print("Sheena: Yes daddy, I will make sure to never leave your sight! Now can we go dad!")
+print("Narrator: They set off for a walk not knowing that this might be their last.")
+
+
+
 
 # This is where the actual game will run from
 class Game_engine(object):
@@ -22,48 +31,55 @@ class Game_engine(object):
         while True:
 
             # This will be used later if the user has a gun or not.
-            a = 0
+            guns_or_noguns = 0
 
             # The path the demon will be on - randomizes every round
             demon_path = random.choice(self.paths)
 
 
             # Randomly assign the item path
-            item_paths = [path for path in self.paths if path != demon_path]
-            item_path = random.choice(item_paths)
+            item_paths = random.choice([path for path in self.paths if path != demon_path])
 
 
             # This is where the lore is
-            lore_paths = [path for path in self.paths if (path != demon_path and path not in item_path)]
-            lore_path = random.choice(lore_paths)
+            lore_path = random.choice([path for path in self.paths if (path != demon_path and path not in item_paths)])
 
-
-            # Ask the user if they want to craft an item.
-            need_help = input(str("\nWould you like to craft an item? (y/n) -> "))
-
-            # If the user picks an option that is not y or n, they have to repick.
-            try:
-                if need_help.lower() != "y" and need_help.lower() != "n":
-                    raise ValueError("Invalid input. Please pick 'y' or 'n'.")
-            except ValueError as e:
-                print(str(e))
-                continue
 
             # The user can craft an item if they have all the required parts for it. If they click y the they will be prompted to pick what item they want to craft.
-            # If the user picks n then it will just displaty their current bagpack.
-            if need_help.lower() == "y":
-                print("\nPick an item you would like to craft.")
-                pick_an_item = input(str("1.----->   Torch \n2.----->   Pistol \n==> "))
-                print()
-                if pick_an_item == "1":
-                    self.father.create_torch(self.item.users_bagpack)
-                elif pick_an_item == "2":
-                    self.father.create_gun(self.item.users_bagpack)
-                else:
-                    print("You did not pick one of the options given!")
-            elif need_help.lower() == "n":
-                print("You have these items on your bagpack:", self.item.users_bagpack)
-                print()
+            # If the user picks n then it will just display their current bagpack.
+            while True:
+
+                # Ask the user if they want to craft an item.
+                need_help = input(str("\nIf you need to see the crafting manual, press (h) \nWould you like to craft an item? (y/n)\n=> "))
+
+                # If the user picks an option that is not y or n, they have to repick.
+                try:
+                    if need_help.lower() != "y" and need_help.lower() != "n" and need_help.lower() != "h":
+                        raise ValueError("Invalid input. Please pick 'y', 'n', or 'h'.")
+                except ValueError as e:
+                    print(str(e))
+                    continue
+
+                if need_help.lower() == 'h':
+                    print("\nFlashlight -> You already have a flashlight, you just need to collect [Batteries] to use it = Reveals the demon path\n")
+                    print("Torch -> You need [Matches] & [Wooden stick] = reveals what path the item is on\n")
+                    print("Deagle -> You need [Pistol] & [Gunpowder] & [Magazine] = You can kill the demon but only if you have [Bullets]\n")
+                elif need_help.lower() == "y":
+                    print("\nPick an item you would like to craft.")
+                    pick_an_item = input(str("1.----->   Torch \n2.----->   Pistol \n==> "))
+                    print()
+                    if pick_an_item == "1":
+                        self.father.create_torch(self.item.users_bagpack)
+                        break
+                    elif pick_an_item == "2":
+                        self.father.create_gun(self.item.users_bagpack)
+                        break
+                    else:
+                        print("You did not pick one of the options given!")
+                elif need_help.lower() == "n":
+                    print("You have these items on your bagpack:", self.item.users_bagpack)
+                    print()
+                    break
 
             # The user will be asked if they would like to use an item, if they pick y then they will choose what item,
             # If they pick n then they will n, it will continue on with the game.
@@ -102,14 +118,14 @@ class Game_engine(object):
             loop_count += 1
 
             # This if will run if the user did not use the gun to kill the demon
-            if a == 0:
-                print("Becareful and choose the right path\n")
+            if guns_or_noguns == 0:
+                print("Be careful and choose the right path\n")
                 while True:
                     try:
 
                         # If the user picks a path number that is not valid, they have to re-pick
-                        user_choice = int(input(f"Tunnel #{loop_count} "
-                                                f"Walk your path => "))
+                        user_choice = int(input(f"Round #{loop_count} "
+                                                f"Choose your path => "))
                         if user_choice < 1 or user_choice > 3:
                             raise ValueError(f" There is no {user_choice}, please pick a valid path! \n")
                         break
@@ -124,7 +140,7 @@ class Game_engine(object):
                     self.demon.death()
 
                 # If the user picks this path it will call the item class.
-                elif chosen_path == item_path:
+                elif chosen_path == item_paths:
                     self.item.collect_item()
 
 
@@ -133,7 +149,7 @@ class Game_engine(object):
                     self.lore.lore_play()
 
             # This if will run if the user does have a gun to kill the demon.
-            if a == 1:
+            if guns_or_noguns == 1:
                 print("You have a loaded Deagle, aim wisely!\n")
                 while True:
                     try:
@@ -154,7 +170,7 @@ class Game_engine(object):
                     self.demon.kill_demon()
 
                 # If the user picks this path it will call the item class.
-                elif chosen_path == item_path:
+                elif chosen_path == item_paths:
                     self.item.collect_item()
                     self.demon.missed_demon()
 
