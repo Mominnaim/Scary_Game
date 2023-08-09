54e9c9b931d0a0892f6e862c082ec963f1f37e3f
Naim Momin <mominnaim11@gmail.com>
2023-08-09 15:54:37 -0500

    Updated the test file

--- test_skeleton.py
+++ test_skeleton.py
@@ -1,19 +1,16 @@
 import random
 
-from .scary_game import Father,Item
-
+from .scary_game import Father, Item
 
 item_list = ["Battery", "Matches", "Wooden stick", "Bullets", "Magazine", "Pistol", "Gunpowder"]
 
 lore_none = []
 battery = "Battery"
-torch = "Torch"
 deagle = "Deagle"
 matches = "Matches"
-woodenstick = "Wooden stick"
 bullets = "Bullets"
 magazine = "Magazine"
-pistol ="Pistol"
+pistol = "Pistol"
 gunpowder = "Gunpowder"
 
 
@@ -21,44 +18,34 @@ def test_father_one():
     Dad = Father()
     users_bagpack_2 = ["Battery"]
     demon = 1
-    Dad.use_item(users_bagpack_2,demon)
+    Dad.use_item(users_bagpack_2, demon)
     assert len(users_bagpack_2) == 0
 
 
 def test_father_two():
     Dad = Father()
-    user_bp = [woodenstick,matches]
-    Dad.create_torch(user_bp)
-    assert torch in user_bp
+    users_bagpack_3 = [matches]
+    item = 3
+    Dad.use_fire_lamp(users_bagpack_3, item)
+    assert len(users_bagpack_3) == 0
 
 
 def test_father_three():
     Dad = Father()
-    users_bagpack_1 = [pistol,magazine,gunpowder]
-    Dad.create_gun(users_bagpack_1)
-    assert deagle in users_bagpack_1
-
-def test_father_four():
-    Dad = Father()
-    users_bagpack_3 = [torch]
-    item = 3
-    Dad.use_torch(users_bagpack_3,item)
-    assert len(users_bagpack_3) == 0
-
-
-def test_father_five():
-    Dad = Father()
     gun_bagpack = [bullets, deagle]
     Dad.use_pistol(gun_bagpack)
     assert len(gun_bagpack) == 1
+
+
+def test_father_four():
+    dad = Father()
+    backpack = [magazine, pistol]
+    dad.create_gun(backpack)
+    assert len(backpack) == 1
+
 
 def test_item_one():
     empty_bagpack = []
     pieces = Item(item_list, empty_bagpack)
     pieces.collect_item()
     assert len(empty_bagpack) == 1
-
-
-
-
-    
